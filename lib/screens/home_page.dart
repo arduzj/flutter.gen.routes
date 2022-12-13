b9843c23bf0d642dc6e5396905da3f0aa63090de
Javier Arduz Lopez <arduzj@gmail.com>
2022-12-12 20:27:07 -0400

    Basic changes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -34,7 +34,7 @@ class HomePage extends StatelessWidget {
                 }
               ),
 
-              const SizedBox(height: 20,),
+              const SizedBox(height: 30,),
 
               //ABOUT PAGE button
               TextButton(
